18570cb26a68ec20225db51ad6adec8e05445039
Matheus <matheus@email.com>
2026-04-17 20:12:16 -0300

    fix: adiciona import de Sum e Avg

--- league/views.py
+++ league/views.py
@@ -9,6 +9,7 @@ from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from django.views.decorators.http import require_POST
+from django.db.models import Sum, Avg
 
 from .models import (
     Draft,
